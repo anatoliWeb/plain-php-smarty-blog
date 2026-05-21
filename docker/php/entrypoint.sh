69a0c5032ba18084c5b713d956128e709faaf8f1
tolla <anatolii.web@gmail.com>
2026-05-21 18:35:15 +0300

    fix: normalize docker entrypoint line endings

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-if [ ! -f .env ] && [ -f .env.example ]; then
+if [ ! -f ".env" ] && [ -f ".env.example" ]; then
   cp .env.example .env
 fi
 
-if [ ! -d vendor ]; then
+if [ ! -d "vendor" ]; then
   composer install --no-interaction --prefer-dist
 fi
 
